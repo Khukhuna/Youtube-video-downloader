421742d47fc15d44da6982b1203682b4f57a7f35
FiNkacho <sabaxuxunashvili@gmail.com>
2018-09-05 22:43:00 +0400

    Details page w/ youtube video player

--- static/js/getData.js
+++ static/js/getData.js
@@ -5,7 +5,24 @@ function getDataById(id){
         dataType: "json",
         url: url,
         success: function(data) {
-            console.log(data['title']);
+          $("#loading").fadeOut();
+          $("#details").fadeIn();
+          $("#title").text(data['title']);
+          $("#views").text(data['views']);
+          // WARNING: TYPO in API (resoltions must be change to 'resolutions')
+          for (var btn in data['resoltions']) {
+            var resolution = data['resoltions'][btn]['resolution'];
+            var title = 'Download - '+resolution;
+            var $downloadBtn = $('<button />', {
+              type: 'button',
+              text: title,
+              class: 'main-btn'
+            }).on('click', function(){
+              $(location).attr('href', '/download/'+id+'/'+resolution);
+            });
+            $(".details-wrapper").append($downloadBtn);
+          }
+          player.loadVideoById(id);
         },
         error: function(err){
           console.log(err);
---
+++ static/js/youtube-player.js
@@ -0,0 +1,28 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: $(window).height()/2,
+        width: '75%',
+        videoId: 'M7lc1UVf-VE',
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
